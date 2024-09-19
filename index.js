a5b2a0844a86da5b4b3411b1d0d71237c0049dc5
max4ever <maxim.postoronca@gmail.com>
2024-09-19 15:16:48 +0000

    export AbstractRepository

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ import HashedSet from './src/Sets/HashedSet.js';
 import StartHelper from './src/StartHelper.js';
 import UrlHash from './src/Sets/UrlHash.js';
 import MongoDbConnector from './src/mongodb/MongoDbConnector.js';
+import AbstractRepository from './src/mongodb/AbstractRepository.js';
 
-export { DatasetUtils, HashedSet, StartHelper, UrlHash, MongoDbConnector };
+export { DatasetUtils, HashedSet, StartHelper, UrlHash, MongoDbConnector, AbstractRepository };
