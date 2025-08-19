2c7fc59ccc7421a6120d122edd719276218939ff
max4ever <maxim.postoronca@gmail.com>
2025-08-19 12:21:54 +0000

    fix remove axioshelper

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import DatasetUtils from './src/DatasetUtils.js';
 import HashedSet from './src/Sets/HashedSet.js';
 import StartHelper from './src/StartHelper.js';
-import AxiosHelper from './src/AxiosHelper.js';
 import UrlHash from './src/Sets/UrlHash.js';
 import MongoDbConnector from './src/mongodb/MongoDbConnector.js';
 import AbstractRepository from './src/mongodb/AbstractRepository.js';
 
-export { DatasetUtils, HashedSet, StartHelper, AxiosHelper, UrlHash, MongoDbConnector, AbstractRepository };
+export { DatasetUtils, HashedSet, StartHelper, UrlHash, MongoDbConnector, AbstractRepository };
